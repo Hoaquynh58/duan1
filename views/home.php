fd1f59ce051f4819687864354048e6bbcd58bc41
PhucZuu <phucdinhle2004@gmail.com>
2024-03-19 10:25:01 +0700

    update new code

---
+++ views/home.php
@@ -0,0 +1,78 @@
+    <!-- BANNER -->
+    <section class="home section--lg">
+      <div class="home__container container grid">
+        <div class="home__content">
+          <span class="home__subtitle">Khuyến mãi hấp dẫn</span>
+          <h1 class="home__title">Xu hướng thời trang
+            <span>Bộ sưu tập đặc biệt</span>
+          </h1>
+          <p class="home__description">Tiết kiệm nhiều hơn với phiếu giảm giá và giảm giá tới 20%</p>
+          <a href="shop.html" class="btn">Shop Now</a>
+        </div>
+
+        <img src="./assets/img/home-img.png" alt="" class="home__img">
+      </div>
+    </section>
+
+    <!-- FILTER -->
+    <section class="products section container">
+      <div class="tab__btns">
+        <span class="tab__btn active-tab">Featured</span>
+        <span class="tab__btn">Popular</span>
+        <span class="tab__btn">New added</span>
+      </div>
+      
+      <!-- DANH SÁCH SẢN PHẨM -->
+      <div class=" tab__items">
+          <div class="tab__item active-tab">
+            <div class="products__container grid">
+              <!-- SẢN PHẨM -->
+              <div class="product__item">
+                <div class="product__banner">
+                  <a href="details.html" class="product__imgaes">
+                    <!-- ẢNH CHÍNH -->
+                    <img src="./assets/img/product-1-1.jpg" alt="" class="product__img defaule">
+                  </a>
+
+                  <!-- ICON HOVER -->
+                  <!-- <div class="producut__actions">
+                    <a href="#" class="action__btn" aria-lable="Quick View">
+                      <i class="fa-regular fa-eye"></i>
+                    </a>
+
+                    <a href="#" class="action__btn" aria-lable="Add to Wishlist">
+                      <i class="fa-solid fa-heart"></i>
+                    </a>
+
+                    <a href="#" class="action__btn" aria-lable="Compare">
+                      <i class="fa-solid fa-shuffle"></i>
+                    </a>
+                  </div> -->
+
+                  <!-- SALE/HOT -->
+                  <div class="product__badge light-pink">Hot</div>
+                </div>
+
+                <div class="product_content">
+                  <span class="product__category">Clothing</span>
+                  <a href="details.html">
+                    <h3 class="product__title">Colorful Pattern Shirts</h3>
+                  </a>
+                  
+                  <div class="product__price flex">
+                    <span class="new__price">$238.85</span>
+                    <span class="old__price">$240.8</span>
+                  </div>
+
+                  <!-- THÊM VÀO GIỎ -->
+                  <a href="#" class="action__btn cart__btn" aria-lable="Add To Cart">
+                    <i class="fa-solid fa-shop"></i>
+                  </a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+      </div>
+
+    </section>
